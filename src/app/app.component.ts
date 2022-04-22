f644f0ea489c8b74415b21ed48ff46c2baf333b1
Jeison <jeisonmulti@gmail.com>
2022-04-22 15:15:05 -0600

    playing...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,82 @@
 import { Component } from '@angular/core';
 
+import { Product } from './product.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'jk_store';
+  name = 'Jeison';
+  age = 18;
+  img = 'https://www.w3schools.com/howto/img_avatar.png';
+  btnDisabled = true;
+  person = {
+    name: 'Jeison',
+    age: 18,
+    avatar: 'https://www.w3schools.com/howto/img_avatar.png'
+  }
+  names: string[] = ['Jei', 'Juli', 'Santi'];
+  newName = '';
+  products: Product[] = [
+    {
+      name: 'EL mejor juguete',
+      price: 565,
+      image: 'https://source.unsplash.com/random',
+      category: 'all',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: 'https://source.unsplash.com/random'
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: 'https://source.unsplash.com/random'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: 'https://source.unsplash.com/random'
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: 'https://source.unsplash.com/random'
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: 'https://source.unsplash.com/random'
+    }
+  ]
+
+  toggleButton() {
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  increaseAge() {
+    this.person.age += 1;
+  }
+
+  onScroll(event: Event) {
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+  changeName(event: Event) {
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
+
+  addName() {
+    this.names.push(this.newName);
+    this.newName = '';
+  }
+
+  deleteName(index: number) {
+    this.names.splice(index, 1);
+  }
 }
