b039f9f0caa59b26e0e7eb5422cbec620832448b
Jeison Briceño Burgos <jeisonmulti@gmail.com>
2022-06-10 19:45:51 -0600

    reset styles, and created a side bar

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -12,4 +12,10 @@ export class NavComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  isMenuActive: boolean = false;
+
+  toggleStateMenu(): void {
+    this.isMenuActive = !this.isMenuActive;
+  }
+
 }
